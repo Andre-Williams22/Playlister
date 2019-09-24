6095ed8349fd383ceb9b1473c7b1ea33f213162a
Andre <awjrs22@gmail.com>
2019-09-24 14:48:26 -0700

    users can see a single playlist

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def playlists_submit():
         'description': request.form.get('description'),
         'videos': request.form.get('videos').split()
     }
-    playlists.insert_one(playlist)
+    playlist_id = playlists.insert_one(playlist).inserted_id
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
 @app.route('/playlists/<playlist_id>')
